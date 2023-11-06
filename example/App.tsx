902fb5f92192120eaedbf021d5bf3230dfca782d
geekcj12 <geekcj@geekcj.com>
2023-11-06 17:54:47 +0800

    refactor: improve example code

--- example/App.tsx
+++ example/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import { Viewer, Scene, EquirectGeometry, RectilinearView } from '../lib/react-marzipano'
+import { Viewer, Scene, EquirectGeometry, RectilinearView } from 'react-marzipano-component'
 
-function App() {    
+function App() {  
   return (
     <Viewer style={{ position: 'absolute', inset: 0 }}>
       <Scene source="//www.marzipano.net/media/equirect/angra.jpg">
         <EquirectGeometry levelPropertiesList={[{ width: 4000 }]} />
-        <RectilinearView params={{ yaw: Math.PI }} />
+        <RectilinearView
+          params={{ yaw: Math.PI }}
+          limiters={(limit) => ([
+            limit.traditional(1024, 100 * Math.PI / 180)
+          ])}
+        />
       </Scene>
     </Viewer>
   )
