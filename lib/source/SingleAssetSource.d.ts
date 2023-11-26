d976c6685b582eb6786cdb0bfbc3c00b94f779aa
geekcj12 <geekcj@geekcj.com>
2023-11-27 01:59:23 +0800

    chore: delete build output

--- lib/source/SingleAssetSource.d.ts
+++
@@ -1,6 +0,0 @@
-import { Asset } from "marzipano";
-interface SingleAssetSourceProps {
-    asset: Asset;
-}
-export default function SingleAssetSource({ asset }: SingleAssetSourceProps): null;
-export {};
