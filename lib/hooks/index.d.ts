ee18f16eb1458c77b61bcd8834fac92ba6a7b124
geekcj12 <geekcj@geekcj.com>
2026-03-03 21:38:44 +0800

    chore: remove lib/ from version control
    Add lib/ to .gitignore and untrack all build artifacts.
    Build outputs will be generated by CI during publish.

--- lib/hooks/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as useViewer } from './useViewer';
-export { default as useCreateGeometry } from './useCreateGeometry';
-export { default as useViewerContext } from './useViewerContext';
-export { default as useSceneContext } from './useSceneContext';
--- lib/hooks/useCreateGeometry.d.ts
+++
@@ -1,6 +0,0 @@
-interface useCreateGeometryProps {
-    type: 'cube' | 'equirect' | 'flat';
-    levelPropertiesList: Record<string, unknown>[];
-}
-export default function useCreateGeometry({ type, levelPropertiesList }: useCreateGeometryProps): void;
-export {};
